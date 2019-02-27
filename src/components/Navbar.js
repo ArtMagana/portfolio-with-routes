a0833791cbcea38b347a783b4580d724b65405e3
Arturo <arturo.magana@shq.com.mx>
2019-02-26 21:00:53 -0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+// components/Navbar.js
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const navbar = () => {
+  return (
+    <nav className="nav-style">
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/projects' style={{ textDecoration: 'none' }}>Projects</Link></li>
+</ul>
+    </nav>
+  )
+}
+
+export default navbar;
